656f63f9089b9ff80a10a2062a08e473bd6c6382
Francisco Depascuali <francisco.depascuali@gmail.com>
2017-09-11 03:06:08 +0200

    Fixing encoding and adding tests

---
+++ tests/test_suite/encoding_test.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import directory
+import wikiquotes
+import language_manager
+import spanish
+import english
+import Author
+
+class EncodingTest(unittest.TestCase):
+
+    def test_english_encoding(self):
+        self.assertEqual(english, _generate("English"))
+        self.assertEqual(english, _generate("english"))
+        self.assertEqual(english, _generate("en"))
+        # self.assertEqual(english, _generate("Inglés"))
+        self.assertEqual(english, _generate("Ingles"))
+        self.assertEqual(english, _generate("ingles"))
+
+        self.assertEqual(english, _generate(u"English"))
+        self.assertEqual(english, _generate(u"english"))
+        self.assertEqual(english, _generate(u"en"))
+        self.assertEqual(english, _generate(u"Inglés"))
+        self.assertEqual(english, _generate(u"Ingles"))
+        self.assertEqual(english, _generate(u"ingles"))
+
+    def test_spanish_encoding(self):
+
+        self.assertEqual(spanish, _generate("Spanish"))
+        self.assertEqual(spanish, _generate("spanish"))
+        self.assertEqual(spanish, _generate("es"))
+        self.assertEqual(spanish, _generate("Español"))
+        self.assertEqual(spanish, _generate("español"))
+
+        self.assertEqual(spanish, _generate(u"Spanish"))
+        self.assertEqual(spanish, _generate(u"spanish"))
+        self.assertEqual(spanish, _generate(u"es"))
+        self.assertEqual(spanish, _generate(u"Español"))
+        self.assertEqual(spanish, _generate(u"español"))
+
+    def test_get_quotes_encoding(self):
+        author = Author.random_author()
+        quotes = wikiquotes.get_quotes(author.name, author.language)
+
+        for quote in quotes:
+            self.assertTrue(language_manager.is_unicode(quote))
+
+    def test_random_quote_encoding(self):
+        author = Author.random_author()
+        random_quote = wikiquotes.random_quote(author.name, author.language)
+
+        self.assertTrue(language_manager.is_unicode(random_quote))
+
+    def test_quote_of_the_day_encoding(self):
+        spanish_quote_of_the_day = wikiquotes.quote_of_the_day("spanish")
+        self.assertTrue(language_manager.is_unicode(spanish_quote_of_the_day[0]))
+        self.assertTrue(language_manager.is_unicode(spanish_quote_of_the_day[1]))
+
+        english_quote_of_the_day = wikiquotes.quote_of_the_day("english")
+        self.assertTrue(language_manager.is_unicode(english_quote_of_the_day[0]))
+        self.assertTrue(language_manager.is_unicode(english_quote_of_the_day[1]))
+
+    def test_spanish_quote_of_the_day_encoding_invariance(self):
+
+        quote_of_the_day = wikiquotes.quote_of_the_day("spanish")
+
+        self.assertEqual(quote_of_the_day, _qotd("Spanish"))
+        self.assertEqual(quote_of_the_day, _qotd("spanish"))
+        self.assertEqual(quote_of_the_day, _qotd("es"))
+        self.assertEqual(quote_of_the_day, _qotd("Español"))
+        self.assertEqual(quote_of_the_day, _qotd("español"))
+
+        self.assertEqual(quote_of_the_day, _qotd(u"Spanish"))
+        self.assertEqual(quote_of_the_day, _qotd(u"spanish"))
+        self.assertEqual(quote_of_the_day, _qotd(u"es"))
+        self.assertEqual(quote_of_the_day, _qotd(u"Español"))
+        self.assertEqual(quote_of_the_day, _qotd(u"español"))
+
+    def test_english_quote_of_the_day_encoding_invariance(self):
+
+        quote_of_the_day = wikiquotes.quote_of_the_day("english")
+
+        self.assertEqual(quote_of_the_day, _qotd("English"))
+        self.assertEqual(quote_of_the_day, _qotd("english"))
+        self.assertEqual(quote_of_the_day, _qotd("en"))
+        self.assertEqual(quote_of_the_day, _qotd("Inglés"))
+        self.assertEqual(quote_of_the_day, _qotd("Ingles"))
+        self.assertEqual(quote_of_the_day, _qotd("ingles"))
+
+        self.assertEqual(quote_of_the_day, _qotd(u"English"))
+        self.assertEqual(quote_of_the_day, _qotd(u"english"))
+        self.assertEqual(quote_of_the_day, _qotd(u"en"))
+        self.assertEqual(quote_of_the_day, _qotd(u"Inglés"))
+        self.assertEqual(quote_of_the_day, _qotd(u"Ingles"))
+        self.assertEqual(quote_of_the_day, _qotd(u"ingles"))
+
+def _generate(string):
+    return language_manager.LanguageManager(string).language
+
+def _qotd(string):
+    return wikiquotes.quote_of_the_day(string)
+
+if __name__ == '__main__':
+    unittest.main()
--- wikiquotes/managers/language_manager.py
+++ wikiquotes/managers/language_manager.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import directory
 import unidecode
 
 import english
@@ -9,16 +10,31 @@ import custom_exceptions
 class LanguageManager:
 
     def __init__(self, language_string):
+        if not is_unicode(language_string):
+            language_string = unicode(language_string)
         self.language = __get_language__(language_string)
 
 def __get_language__(language_string):
-    lowered = strip_accents(language_string.lower())
-    if lowered == "en" or lowered == "english" or lowered == "ingles":
+    lowered = strip_special_characters(language_string.lower())
+    if lowered == u"en" or lowered == u"english" or lowered == u"ingles":
         return english
-    elif lowered == "es" or lowered == "spanish" or lowered == "español":
+    # Using espanol because special character replaces accents and ñ
+    elif lowered == u"es" or lowered == u"spanish" or lowered == u"espanol":
         return spanish
 
     raise custom_exceptions.UnsupportedLanguageException()
 
-def strip_accents(accented_string):
-    return unidecode.unidecode(accented_string)
+def transform_to_unicode(string):
+    return string if is_unicode(string) else unicode(string)
+
+def strip_special_characters(accented_string):
+    if is_unicode(accented_string) or directory.python_version[0] == 3:
+        return unidecode.unidecode(accented_string)
+    else:
+        return accented_string
+
+def is_unicode(string):
+    if directory.python_version[0] == 2:
+        return isinstance(string, unicode)
+
+    return True

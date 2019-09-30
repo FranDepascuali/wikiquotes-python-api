9f807e9c8aca4e612f29e0b96650855fb96b2987
lubeve <lucasbenac@gmail.com>
2019-09-30 08:12:10 +0200

    Update spanish.py
    The url in the official repository is not updated anymore so I created a modified file to grab the quotes form the "Main page" (Portada)

--- wikiquotes/languages/spanish.py
+++ wikiquotes/languages/spanish.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 base_url = "https://es.wikiquote.org/w/api.php"
-quote_of_the_day_url = "https://es.wikiquote.org/wiki/Wikiquote:Frase_del_día"
+quote_of_the_day_url = "https://es.wikiquote.org/wiki/Portada"
 
 def quote_of_the_day_parser(html):
-    table = html("table")[1].table
-    td = table("td")
-    quote = td[2].text.strip()
-    author = td[-1].div.a.text.strip()
+    table = html("table")[1]("table")[0]
+    quote = table("td")[3].text.strip()
+    author = table("td")[5].div.a.text.strip()
 
     # Author and quote could have an accent
     return (quote, author)
